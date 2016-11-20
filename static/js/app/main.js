4da98c214743aff211d80efd34cb453ed20bc1e6
abbott221 <abbott.221@osu.edu>
2016-11-20 00:20:58 -0500

    add file structure and canvas

---
+++ static/js/app/main.js
@@ -0,0 +1,22 @@
+// This file hosts the main game module.  Whenever it makes sense to
+// put something in its own module, you can either put it in the dependency
+// array for this module (i.e. the way we're using 'game-shim' in th commented
+// out version below), or you can call require() directly closer to the
+// time you need it.  <http://requirejs.org/> has detailed documentation.  
+//
+//
+//
+define(function(require) {
+
+  // Toji's game-shim module is included with this template; if you wish to use
+  // it, uncomment the require() line below.
+  //
+  // The reason that this isn't done by default is that the replacement
+  // versions of the various APIs that it includes could potentially cause
+  // complications or performance issues with other libraries that use these APIs
+  // and may have their own strategies for dealing with cross-browser issues.  
+  // We suggest reviewing the code (in lib/game-shim.js) before enabling it.
+  //
+  //require('game-shim');
+  
+});
